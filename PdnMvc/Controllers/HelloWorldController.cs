e9fae8b14d55e740674ec36ca50c9839f208ee58
Justin Aurand <justin.aurand@energage.com>
2019-10-18 09:55:00 -0400

    add api controller and readme containing endpoints

---
+++ PdnMvc/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿namespace PdnMvc.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "Default 'Hello'.";
+        }
+
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome()
+        {
+            return "Hello and welcome!";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Endpoints
+
+GET https://localhost:44314/helloworld
+GET https://localhost:44314/helloworld/welcome
